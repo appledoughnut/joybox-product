14658a2d38eb3ca67fe575fc11e0744457be1457
jonghoon <>
2022-09-26 20:21:09 +0900

    코틀린 프로젝트 생성

---
+++ src/test/kotlin/app/joybox/product/ProductApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.joybox.product
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
