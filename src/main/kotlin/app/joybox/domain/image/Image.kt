96440cb51eee0047fdab26d220f6228a56484e4b
Jonghyeok Lee <>
2022-10-02 14:01:21 +0900

    Image, Product entity 구현

--- src/main/kotlin/app/joybox/domain/image/Image.kt
+++ src/main/kotlin/app/joybox/domain/image/Image.kt
@@ -2,17 +2,14 @@ package app.joybox.domain.image
 
 import app.joybox.domain.product.Product
 import org.springframework.data.annotation.CreatedDate
-import java.io.InputStream
 import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 @Table(name="product_images")
 data class Image(
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long?,
-        val name: String,
-        @CreatedDate
-        val createdAt: LocalDateTime,
-        @ManyToOne
+        @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) val id: Long,
+        @Column(name = "name", nullable = false) val name: String,
+        @Column(name = "created_at", nullable = false) @CreatedDate val createdAt: LocalDateTime,
+        @JoinColumn(name = "product_id") @ManyToOne val product: Product
 )
--- src/main/kotlin/app/joybox/domain/product/Product.kt
+++ src/main/kotlin/app/joybox/domain/product/Product.kt
@@ -1,13 +1,15 @@
 package app.joybox.domain.product
 
+
+import app.joybox.domain.image.Image
 import javax.persistence.*
 
 @Entity
 @Table(name = "products")
 class Product(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long?,
-    @Column(name = "title") val title: String,
-    @Column(name = "price") val price: Int,
-    @Column(name = "description") val description: String,
-    @Column(name = "")
+    @Column(name = "title", nullable = false) val title: String,
+    @Column(name = "price", nullable = false) val price: Int,
+    @Column(name = "description") @Lob val description: String,
+    @Column(name = "images") @OneToMany(mappedBy = "product") val images: List<Image>
 )
