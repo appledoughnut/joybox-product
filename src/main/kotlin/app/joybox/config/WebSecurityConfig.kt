03977ee788dcb4bcb489e252706e335923e4ebfe
Jonghyeok Lee <>
2022-10-16 12:33:54 +0900

    merge from main branch

---
+++ src/main/kotlin/app/joybox/config/WebSecurityConfig.kt
@@ -0,0 +1,12 @@
+package app.joybox.config
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+
+class WebSecurityConfig: WebSecurityCustomizer {
+
+    override fun customize(web: WebSecurity?) {
+    }
+
+
+}
--- src/main/kotlin/app/joybox/domain/image/Image.kt
+++ src/main/kotlin/app/joybox/domain/image/Image.kt
@@ -2,28 +2,50 @@ package app.joybox.domain.image
 
 import app.joybox.domain.product.Product
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.domain.Persistable
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.*
 
 @Entity
-@Table(name="product_images")
-@EntityListeners( AuditingEntityListener::class )
-class Image(id: UUID? = null) {
+@Table(name = "product_images")
+@EntityListeners(AuditingEntityListener::class)
+class Image: Persistable<UUID> {
     @Id
-    var id: UUID? = id; private set
+    @Column(columnDefinition = "uuid")
+    var uuid: UUID? = null; private set
 
     @Column(name = "name")
     // TODO: @NotNull
     // 확인 필요
-    var name: String = ""
+    var name: String = ""; private set
 
     @JoinColumn(name = "product_id")
     @ManyToOne
-    var product: Product? = null
+    var product: Product? = null; private set
 
     @Column(name = "created_at")
     @CreatedDate
-    var createdAt: LocalDateTime = LocalDateTime.MIN
+    var createdAt: LocalDateTime? = null; private set
+
+    companion object {
+        fun create(
+            id: UUID?,
+            name: String
+        ): Image {
+            val image = Image()
+            image.uuid = id
+            image.name = name
+            return image
+        }
+    }
+
+    override fun getId(): UUID? {
+        return this.uuid
+    }
+
+    override fun isNew(): Boolean {
+        return this.createdAt == null
+    }
 }
